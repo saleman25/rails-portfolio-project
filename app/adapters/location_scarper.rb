40631f6e9f9e4a2785662342e75094f00ccf5f68
Samantha Aleman <samanthaaleman@Samanthas-MacBook-Air.local>
2020-10-11 23:26:38 -0700

    added more to scraper

--- app/adapters/location_scarper.rb
+++ app/adapters/location_scarper.rb
@@ -6,12 +6,17 @@ class LocationScraper
 
     def scrape_locations
 
-        doc = Nokogiri::HTML(open())
+        doc = Nokogiri::HTML(open(https://simple.wikipedia.org/wiki/List_of_countries))
         attrs_arr = []
-        doc.css().each do |row| 
+        doc.css('table.wikitable.sortable').each do |row| 
             attrs = {}
             if !row.css('td').empty?
-                attrs[:country]
-                attrs[:state]
-                attrs[:city]
+                attrs[:country] = row.css('td').first.text.split("")[0].strip
+            end 
+            attrs_arr << attrs 
+        end
+    end
+    File.write('country.json' , attr_arr.to_json)
+end 
+end 
                 
