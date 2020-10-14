403943232cf76bce1590628344948e3a6b7cd1ab
Samantha Aleman <samanthaaleman@Samanthas-MacBook-Air.local>
2020-10-14 13:11:02 -0700

    fixing nested routes

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,20 +1,24 @@
 class VisitsController < ApplicationController
 
 def new 
-
+get_vacation
 end 
 
 def index 
-    @vacation = Vacation.find_by(id: params[:vacation_id])
-    
+   get_vacation 
+   @visit = Visit.new 
+
 end 
 
 def create 
 
 end 
 
+private 
 
-
+def get_vacation 
+    @vacation = Vacation.find_by(id: params[:vacation_id])
+end 
 
 
 
